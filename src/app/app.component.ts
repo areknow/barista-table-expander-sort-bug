f33f3e3f22c5a5ac20d58a6e62de3ebff33595af
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-07-22 17:01:18 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'a-table-expander-demo';
+  dataSource: any[] = [
+    {
+      host: 'et-demo-2-win4',
+      cpu: '30 %',
+      memory: '38 % of 5.83 GB',
+      traffic: '98.7 Mbit/s'
+    },
+    {
+      host: 'et-demo-2-win3',
+      cpu: '26 %',
+      memory: '46 % of 6 GB',
+      traffic: '625 Mbit/s',
+      expanded: true
+    },
+    {
+      host: 'docker-host2',
+      cpu: '25.4 %',
+      memory: '38 % of 5.83 GB',
+      traffic: '419 Mbit/s'
+    },
+    {
+      host: 'et-demo-2-win1',
+      cpu: '23 %',
+      memory: '7.86 % of 5.83 GB',
+      traffic: '98.7 Mbit/s'
+    }
+  ];
+
+  sortData(event): void {
+    const data = this.dataSource.slice();
+    data.sort((a, b) => {
+      const isAsc = event.direction === 'asc';
+      switch (event.active) {
+        case 'host':
+          return this.compare(a.host, b.host, isAsc);
+        case 'cpu':
+          return this.compare(a.cpu, b.cpu, isAsc);
+        case 'memory':
+          return this.compare(a.memoryPerc, b.memoryPerc, isAsc);
+        case 'traffic':
+          return this.compare(a.traffic, b.traffic, isAsc);
+        default:
+          return 0;
+      }
+    });
+    this.dataSource = data;
+  }
+  compare(a: number | string, b: number | string, isAsc: boolean): number {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DtIconModule } from '@dynatrace/barista-components/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DtTableModule } from '@dynatrace/barista-components/table';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        DtIconModule.forRoot({ svgIconLocation: '/assets/icons/{{name}}.svg' }),
+        DtTableModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
