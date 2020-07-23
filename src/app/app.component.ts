fb64bfa2032ae9b693a380b618122fc556ea0337
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-07-23 08:48:14 -0400

    moved quick filter demo to another repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,4 @@
 import { Component } from '@angular/core';
-import { isObject } from '@dynatrace/barista-components/core';
-import {
-  DtQuickFilterDefaultDataSource,
-  DtQuickFilterDefaultDataSourceConfig,
-} from '@dynatrace/barista-components/experimental/quick-filter';
-
-export const FILTER_FIELD_CONFIG = {
-  autocomplete: [
-    {
-      name: 'Account type',
-      distinct: true,
-      autocomplete: [{ name: 'a'},{ name: 'b'},{ name: 'c'}]
-    },
-    {
-      name: 'Support type',
-      distinct: true,
-      autocomplete: [{ name: 'x'},{ name: 'y'},{ name: 'z'}]
-    }
-  ]
-};
 
 @Component({
   selector: 'app-root',
@@ -53,30 +33,6 @@ export class AppComponent {
       traffic: '98.7 Mbit/s'
     }
   ];
-
-  /** configuration for the quick filter */
-  private config: DtQuickFilterDefaultDataSourceConfig = {
-    // Method to decide if a node should be displayed in the quick filter
-    showInSidebar: (node) =>
-      isObject(node) && node.name && node.name !== 'Not in Quickfilter',
-  };
-
-  filterSource = new DtQuickFilterDefaultDataSource(
-    FILTER_FIELD_CONFIG,
-    this.config,
-  );
-
-  fakeFilters = [
-    [
-      { name: 'Group 1', distinct: true, autocomplete: [{ name: 'a' }, { name: 'b' }, { name: 'c' }] },
-      { name: 'b' }
-    ],
-    [
-      { name: 'Group 2', distinct: true, autocomplete: [{ name: 'x' }, { name: 'y' }, { name: 'z' }] },
-      { name: 'y' }
-    ]
-  ];
-
 
   sortData(event): void {
     const data = this.dataSource.slice();
